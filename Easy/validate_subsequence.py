6bf75ea308cf90346610e61069da5c1f4e936096
Saby007 <sabyanew@gmail.com>
2024-09-12 11:20:31 +0530

    Update validate_subsequence.py

--- Easy/validate_subsequence.py
+++ Easy/validate_subsequence.py
@@ -9,6 +9,15 @@ def isValidSubsequence(array,sequence):
     
     return True
 
+def isValidSubsequence2(array, sequence):
+    seqInd = 0
+    for value in array:
+        if seqInd == len(sequence):
+            break
+        if sequence[seqInd] == value:
+            seqInd += 1
+    return seqInd == len(sequence)
+
 array = [5,1,22,25,6,-1,8,10]
 sequence = [5,1,25,22,6,-1,8,10]
 
