a92a0d248709df0020e83960a93423d35f8b9835
Saby007 <sabyanew@gmail.com>
2024-09-12 11:32:33 +0530

    Update two_number_sum.py

--- Easy/two_number_sum.py
+++ Easy/two_number_sum.py
@@ -10,6 +10,21 @@ def twoNumberSum(array, targetSum):
                     break
     return []
 
+def twoNumberSum2(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right -= 1
+    return []
+        
+
 
 array = [3,5,-4,8,11,1,-1,6]
 targetSum = 10
